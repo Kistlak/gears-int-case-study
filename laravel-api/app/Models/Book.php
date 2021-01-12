cd9085697814b5bdb96406d366a9f82ec292a04c
Kistlak Rajapaksha <kistlakall@gmail.com>
2021-01-12 23:55:04 +0530

    laravel config and factory and seeder and api login created

---
+++ laravel-api/app/Models/Book.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'book_name',
+        'description',
+        'price'
+    ];
+
+    public function authors()
+    {
+        return $this->hasOne(User::class);
+    }
+}
